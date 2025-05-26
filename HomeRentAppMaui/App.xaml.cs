0e5702b6dc1e5a833094eeab2d10652c3c261989
eduardo <eduardo@DESKTOP-5015NEQ>
2025-05-25 20:08:34 -0500

    Add project files.

---
+++ HomeRentAppMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeRentAppMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HomeRentAppMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeRentAppMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
